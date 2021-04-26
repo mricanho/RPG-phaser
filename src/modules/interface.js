9cb442ac7318e4863c8a45d53273fb684ccf4156
Miguel Ricaño <mricanho@gmail.com>
2021-04-26 10:31:17 -0500

    Add menu folder and menuitem.js as a class

--- src/modules/interface.js
+++ src/modules/interface.js
@@ -1,28 +1,6 @@
 import Phaser from 'phaser';
+import MenuItem from './menus/menuItem'
 
-const MenuItem = new Phaser.Class({
-  Extends: Phaser.GameObjects.Text,
-
-  initialize:
-
-  function MenuItem(x, y, text, scene) {
-    Phaser.GameObjects.Text.call(this, scene, x, y, text, { color: '#ffffff', align: 'left', fontSize: 15 });
-  },
-
-  select() {
-    this.setColor('#f8ff38');
-  },
-
-  deselect() {
-    this.setColor('#ffffff');
-  },
-  // when the associated enemy or player unit is killed
-  unitKilled() {
-    this.active = false;
-    this.visible = false;
-  },
-
-});
 
 // base menu class, container for menu items
 const Menu = new Phaser.Class({
